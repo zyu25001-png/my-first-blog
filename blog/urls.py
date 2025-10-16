eebc8a4ef1d95ffa9fb8c53e28ae030a3f09cc65
z_yu25001 <test@mail.com>
2025-10-16 15:26:33 +0900

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+# URLパターンのリスト
 urlpatterns = [
+    # ホームページ：記事一覧
     path('', views.post_list, name='post_list'),
+
+    # 記事詳細ページ。pkはプライマリキー
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
-
 # Create your views here.
-from django.shortcuts import render
-from .models import Post
-from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from .models import Post  # Postモデルをインポート
+from django.utils import timezone  # 時刻操作用
 
-
+# ブログ記事一覧ページ
 def post_list(request):
+    # 公開日が現在時刻以下の記事を取得して日付順に並べる
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    # post_list.html テンプレートに posts を渡してレンダリング
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+# ブログ記事詳細ページ
+def post_detail(request, pk):
+    # プライマリキー(pk)に一致する記事を取得。存在しない場合は404ページ
+    post = get_object_or_404(Post, pk=pk)
+    # post_detail.html テンプレートに post を渡してレンダリング
+    return render(request, 'blog/post_detail.html', {'post': post})
